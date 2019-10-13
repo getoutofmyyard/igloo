8766161980bb6169700d0376a2768f836938b5e4
Elliott <Jared.Elliott@childrenscolorado.org>
2019-10-13 03:20:11 -0600

    Delete save-time.py

--- command-sets/save-time.py
+++
@@ -1,7 +0,0 @@
-with open('.\\installCmds.txt','r') as file:
-	read_file = file.read()
-	split_file = read_file.split('\n')
-	for line in split_file:
-		split_line = line.split('\'')
-		print(split_line[0])
-		
