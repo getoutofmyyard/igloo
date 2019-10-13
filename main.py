2e53fe0e5ecb60b966dc3a2f627ad541701bfb18
getoutofmyyard <42564339+getoutofmyyard@users.noreply.github.com>
2019-10-12 18:11:05 -0600

    Added interface on/off commands

--- main.py
+++ main.py
@@ -208,17 +208,22 @@ def crypto_tree(crypto_command):
 def int_tree(int_command):
 
     split_cmd = int_command.split(' ')
-    int_index = split_cmd[2]
+    int_index = split_cmd[1]
 
-    if 'enable' in int_command:
-        get_int_list = pshell_decoder('Get-NetAdapter -InterfaceIndex {} | Select-Object Name, ifIndex | Format-List'.format(int_index))
+    if 'on' in int_command:
+        get_int_list = pshell_decoder('Get-NetAdapter -InterfaceIndex {} | Select-Object Name | Format-Table -AutoSize'.format(int_index))
 
-        split_list = get_int_list.split()
-        int_name = split_list[2]
+        split_list = get_int_list.split('----')
+        get_name = split_list[1]
+        int_name = '\'' + get_name.strip() + '\''
 
         enable_adapter = pshell_decoder('Enable-NetAdapter -Name {}'.format(int_name))
 
-        if 'Enable-NetAdapter' in enable_adapter:
+        if 'Enable-NetAdapter : Access is denied.' in enable_adapter:
+        	newline()
+        	print('error~! This action requires admin rights!')
+        	newline()
+        elif 'No MSFT_NetAdapter objects found' in enable_adapter:
             newline()
             print('notify~! Interface does not exist. Use \'show int\' and \'int ?\' for help')
             newline()
@@ -227,18 +232,25 @@ def int_tree(int_command):
             print('notify~! Interface {} has been enabled'.format(int_name))
             newline()
 
-    elif 'disable' in int_command:
+    elif 'off' in int_command:
+
         split_cmd = int_command.split(' ')
-        int_index = split_cmd[2]
+        int_index = split_cmd[1]
 
-        get_int_list = pshell_decoder('Get-NetAdapter -InterfaceIndex {} | Select-Object Name, ifIndex | Format-List'.format(int_index))
+        get_int_list = pshell_decoder('Get-NetAdapter -InterfaceIndex {} | Select-Object Name | Format-Table -AutoSize'.format(int_index))
 
-        split_list = get_int_list.split()
-        int_name = split_list[2]
+        split_list = get_int_list.split('----')
+        get_name = split_list[1]
+        int_name = '\'' + get_name.strip() + '\''
 
         disable_adapter = pshell_decoder('Disable-NetAdapter -Name {} -Confirm:$False'.format(int_name))
 
-        if 'Disable-NetAdapter' in disable_adapter:
+
+        if 'Disable-NetAdapter : Access is denied.' in disable_adapter:
+        	newline()
+        	print('error~! This action requires admin rights!')
+        	newline()
+        elif 'No MSFT_NetAdapter objects found' in disable_adapter:
             newline()
             print('notify~! Interface does not exist. Use \'show int\' and \'int ?\' for help')
             newline()
